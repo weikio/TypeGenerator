dd74e544b61529255e3bca9bfb4a5ff910f5e8ae
Mikael Koskinen <mikael.koskinen@adafy.com>
2021-03-27 12:13:19 +0200

    Added string builder extensions for easier code writing

---
+++ src/Weikio.TypeGenerator/StringBuilderExtensions.cs
@@ -0,0 +1,52 @@
+﻿// ReSharper disable once CheckNamespace
+
+namespace System.Text
+{
+    public static class StringBuilderExtensions
+    {
+        public static void UsingNamespace(this StringBuilder sb, string ns)
+        {
+            sb.AppendLine($"using {ns};");
+        }
+
+        public static void WriteLine(this StringBuilder sb, string text)
+        {
+            sb.AppendLine(text);
+        }
+
+        public static void Write(this StringBuilder sb, string text)
+        {
+            sb.Append(text);
+        }
+
+        public static void Namespace(this StringBuilder sb, string ns)
+        {
+            sb.AppendLine($"namespace {ns}");
+            sb.AppendLine("{");
+        }
+
+        public static void StartBlock(this StringBuilder sb)
+        {
+            sb.AppendLine("{");
+        }
+
+        public static void FinishBlock(this StringBuilder sb)
+        {
+            sb.AppendLine("}");
+        }
+
+        public static void StartClass(this StringBuilder sb, string className)
+        {
+            sb.AppendLine($"public class {className}");
+            sb.AppendLine("{");
+        }
+
+        public static void UsingBlock(this StringBuilder writer, string declaration, Action<StringBuilder> inner)
+        {
+            writer.Write("using (" + declaration + ")");
+            writer.Write("{");
+            inner(writer);
+            writer.FinishBlock();
+        }
+    }
+}
